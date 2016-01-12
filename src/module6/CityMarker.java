e46c5a62bd7cb78574418f0d6939cd3225235511
York <ysmintor@gmail.com>
2016-01-12 14:23:43 +0800

    finish week 4 project
    optional requirement is not implement.

--- src/module6/CityMarker.java
+++ src/module6/CityMarker.java
@@ -7,31 +7,31 @@ import processing.core.PConstants;
 import processing.core.PGraphics;
 
 /** Implements a visual marker for cities on an earthquake map
- * 
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
- * 
+ *
  */
 public class CityMarker extends CommonMarker {
-	
+
 	public static int TRI_SIZE = 5;  // The size of the triangle marker
-	
+
 	public CityMarker(Location location) {
 		super(location);
 	}
-	
-	
+
+
 	public CityMarker(Feature city) {
 		super(((PointFeature)city).getLocation(), city.getProperties());
 		// Cities have properties: "name" (city name), "country" (country name)
 		// and "population" (population, in millions)
 	}
-	
-	
+
+
 	// pg is the graphics object on which you call the graphics
 	// methods.  e.g. pg.fill(255, 0, 0) will set the color to red
-	// x and y are the center of the object to draw. 
+	// x and y are the center of the object to draw.
 	// They will be used to calculate the coordinates to pass
-	// into any shape drawing methods.  
+	// into any shape drawing methods.
 	// e.g. pg.rect(x, y, 10, 10) will draw a 10x10 square
 	// whose upper left corner is at position x, y
 	/**
@@ -41,23 +41,23 @@ public class CityMarker extends CommonMarker {
 		//System.out.println("Drawing a city");
 		// Save previous drawing style
 		pg.pushStyle();
-		
+
 		// IMPLEMENT: drawing triangle for each city
 		pg.fill(150, 30, 30);
 		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
-		
+
 		// Restore previous drawing style
 		pg.popStyle();
 	}
-	
+
 	/** Show the title of the city if this marker is selected */
 	public void showTitle(PGraphics pg, float x, float y)
 	{
 		String name = getCity() + " " + getCountry() + " ";
 		String pop = "Pop: " + getPopulation() + " Million";
-		
+
 		pg.pushStyle();
-		
+
 		pg.fill(255, 255, 255);
 		pg.textSize(12);
 		pg.rectMode(PConstants.CORNER);
@@ -66,20 +66,20 @@ public class CityMarker extends CommonMarker {
 		pg.textAlign(PConstants.LEFT, PConstants.TOP);
 		pg.text(name, x+3, y-TRI_SIZE-33);
 		pg.text(pop, x+3, y - TRI_SIZE -18);
-		
+
 		pg.popStyle();
 	}
-	
+
 	private String getCity()
 	{
 		return getStringProperty("name");
 	}
-	
+
 	private String getCountry()
 	{
 		return getStringProperty("country");
 	}
-	
+
 	private float getPopulation()
 	{
 		return Float.parseFloat(getStringProperty("population"));
